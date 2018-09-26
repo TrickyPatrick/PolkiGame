4ce450358472e3182476c0b8486fe341a5c4799b
Tricky Patrick <43622896+TrickyPatrick@users.noreply.github.com>
2018-09-27 00:05:31 +0200

    Delete lol.lua

--- Release/scripts/scripts/lol.lua
+++
@@ -1,8 +0,0 @@
-local a
-
-function OnEngineLoad()
-	print("____________________________________________________________________")
-	a = createSprite("assets/test.png", 464, 464, 400, 400)
-end
-AddEventHandler("OnEngineLoad", OnEngineLoad)
-
